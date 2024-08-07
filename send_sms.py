fd297010dec2501e42861c2722cfdab0c8b52a44
EshanSingh <53839727+EshanSingh@users.noreply.github.com>
2024-08-07 16:54:56 -0400

    removed debug statements

--- send_sms.py
+++ send_sms.py
@@ -13,6 +13,8 @@ import time
 # Variables for Twilio info
 account_sid = os.getenv("ACCOUNT_SID")
 auth_token = os.getenv("AUTH_TOKEN")
+to_number = os.getenv("TO_NUMBER")
+from_number = os.getenv("FROM_NUMBER")
 
 # URL (Change for class)
 url = 'https://app.testudo.umd.edu/soc/search?courseId=CMSC351&sectionId=&termId=202408&_openSectionsOnly=on&creditCompare=&credits=&courseLevelFilter=ALL&instructor=&_facetoface=on&_blended=on&_online=on&courseStartCompare=&courseStartHour=&courseStartMin=&courseStartAM=&courseEndHour=&courseEndMin=&courseEndAM=&teachingCenter=ALL&_classDay1=on&_classDay2=on&_classDay3=on&_classDay4=on&_classDay5=on'
@@ -26,14 +28,11 @@ soup = BeautifulSoup(page.text, 'html')
 # Create SMS client
 client = Client(account_sid,auth_token)
 
-# Wait (not necessary)
-time.sleep(10);
-
 # Make program run continuously
 while True:
     try:
 
-        # get info off of page
+        # get content off of page
         soup = BeautifulSoup(page.text, 'html')
         currinfo = soup.find("div", {"class": "sections sixteen colgrid"})
         
@@ -42,73 +41,58 @@ while True:
         pos2 = 4
 
         # Get info
-        justin1 = currinfo.select("div:nth-child("+str(pos1)+")")
-        herve1 = currinfo.select("div:nth-child("+str(pos2)+")")
-        # print (justin1)
-        # print (herve1)
+        sectionOne = currinfo.select("div:nth-child("+str(pos1)+")")
+        sectionTwo = currinfo.select("div:nth-child("+str(pos2)+")")
 
 
         # Sleep to give time between scrapes
         time.sleep(30);
 
-        # get new info off of page
+        # get new content off of page
         newSoup = BeautifulSoup(page.text, 'html')
         newInfo = newSoup.find("div", {"class": "sections sixteen colgrid"}) # seats-info-group six columns (OLD)
 
         # Get new info
-        justin2 = newInfo.select("div:nth-child("+str(pos1)+")")
-        herve2 = newInfo.select("div:nth-child("+str(pos2)+")")
-        # print (justin2)
-        # print (herve2)
+        sectionOneNew = newInfo.select("div:nth-child("+str(pos1)+")")
+        sectionTwoNew = newInfo.select("div:nth-child("+str(pos2)+")")
 
-        if justin1 == justin2 and herve1 == herve2:
+        if sectionOne == sectionOneNew and sectionTwo == sectionTwoNew:
             continue
 
         else:
 
             #If website has changed during sleep
-            # print('change')
 
-            # Opening cases (Herve vs Justin)
-            if herve1 != herve2:
-                #Send sms (WORKING)
+            # Change cases
+            if sectionTwo != sectionTwoNew:
                 client.messages.create(
-                to="+12073528818",
-                from_="+18333551367",
-                body="Herve opening"
+                to=to_number,
+                from_=from_number,
+                body="Section 2 opening"
                 )
-                # print('CHANGES TO HERVE')
             else:
-                #Send sms (WORKING)
+                #Send sms
                 client.messages.create(
-                to="+12073528818",
-                from_="+18333551367",
-                body="Justin opening"
+                to=to_number,
+                from_=from_number,
+                body="Section 1 opening"
                 )
-                # print('CHANGES TO JUSTIN')
 
             # Get new info
             soup = BeautifulSoup(page.text, 'html')
             currinfo = soup.find("div", {"class": "sections sixteen colgrid"})
  
-            # wait for 30 seconds
+            # wait for 30 seconds until next check cycle
             time.sleep(30)
             continue
 
     # To handle exceptions
     except Exception as e:
-        print("error")
 
-        #Send sms (WORKING)
+        #Send sms
         client.messages.create(
-        to="+12073528818",
-        from_="+18333551367",
+        to=to_number,
+        from_=from_number,
         body="It broke..."
         )
         break
-
-
-# Commands:
-# git commit -am "Comments" (Stages changes)
-# git push origin main      (Pushes Changes to repository)
-# git pull origin main      (Pulls most recent repository to current setup) DOUBLE CHECK
