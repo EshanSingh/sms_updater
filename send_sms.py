7a6302e9f82ff8824104070aa1956edf633b9167
EshanSingh <53839727+EshanSingh@users.noreply.github.com>
2024-08-07 21:54:06 -0400

    removing fluff

--- send_sms.py
+++ send_sms.py
@@ -1,14 +1,12 @@
 import os
-
-# SMS imports
+from dotenv import load_dotenv
 from twilio.rest import Client
-
-# Web Scraping Imports
 from bs4 import BeautifulSoup
 import requests
-
-# Time import to delay scrapes
 import time
+
+# load enviornment variables
+load_dotenv()
 
 # Variables for Twilio info
 account_sid = os.getenv("ACCOUNT_SID")
@@ -16,24 +14,22 @@ auth_token = os.getenv("AUTH_TOKEN")
 to_number = os.getenv("TO_NUMBER")
 from_number = os.getenv("FROM_NUMBER")
 
+# Create SMS client
+client = Client(account_sid,auth_token)
+
 # URL (Change for class)
 url = 'https://app.testudo.umd.edu/soc/search?courseId=CMSC351&sectionId=&termId=202408&_openSectionsOnly=on&creditCompare=&credits=&courseLevelFilter=ALL&instructor=&_facetoface=on&_blended=on&_online=on&courseStartCompare=&courseStartHour=&courseStartMin=&courseStartAM=&courseEndHour=&courseEndMin=&courseEndAM=&teachingCenter=ALL&_classDay1=on&_classDay2=on&_classDay3=on&_classDay4=on&_classDay5=on'
 
 # Get page
 page = requests.get(url)
 
-# Create html soup
-soup = BeautifulSoup(page.text, 'html')
-
-# Create SMS client
-client = Client(account_sid,auth_token)
 
 # Make program run continuously
 while True:
     try:
 
         # get content off of page
-        soup = BeautifulSoup(page.text, 'html')
+        soup = BeautifulSoup(page.text, features='lxml')
         currinfo = soup.find("div", {"class": "sections sixteen colgrid"})
         
         # Position of section in table NOT INDEX (1 is first element)
@@ -43,14 +39,16 @@ while True:
         # Get info
         sectionOne = currinfo.select("div:nth-child("+str(pos1)+")")
         sectionTwo = currinfo.select("div:nth-child("+str(pos2)+")")
+        
+        
 
 
         # Sleep to give time between scrapes
-        time.sleep(30);
+        time.sleep(10);
 
         # get new content off of page
-        newSoup = BeautifulSoup(page.text, 'html')
-        newInfo = newSoup.find("div", {"class": "sections sixteen colgrid"}) # seats-info-group six columns (OLD)
+        soup = BeautifulSoup(page.text, features='lxml')
+        newInfo = soup.find("div", {"class": "sections sixteen colgrid"})
 
         # Get new info
         sectionOneNew = newInfo.select("div:nth-child("+str(pos1)+")")
@@ -79,11 +77,11 @@ while True:
                 )
 
             # Get new info
-            soup = BeautifulSoup(page.text, 'html')
+            soup = BeautifulSoup(page.text, features='lxml')
             currinfo = soup.find("div", {"class": "sections sixteen colgrid"})
  
-            # wait for 30 seconds until next check cycle
-            time.sleep(30)
+            # wait for 10 seconds until next check cycle
+            time.sleep(10)
             continue
 
     # To handle exceptions
